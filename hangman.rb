876f4453d0f12a0c52d452afdb52bb3ff5496a25
Curt Zimmerman <czimmerman9@gmail.com>
2015-02-01 03:59:51 -0500

    added save/load functionality with yaml. Still need ability to save multiple files.

--- hangman.rb
+++ hangman.rb
@@ -1,12 +1,17 @@
 module Hangman
+	require 'yaml'
 	class Game
 		attr_accessor :word_to_guess, :status, :remaining_turns, :incorrect_guesses
 		def initialize ( args = {} )
 			@word_to_guess = args.fetch(:word_to_guess, get_word_to_guess)
-			@status = Array.new(word_to_guess.length) { "_" }
+			@status = args.fetch(:status, default_status)
 			@remaining_turns = args.fetch(:remaining_turns, 10)
-			@incorrect_guesses = []
+			@incorrect_guesses = args.fetch(:incorrect_guesses, [])
 			begin_game
+		end
+
+		def default_status
+			Array.new(word_to_guess.length) { "_" }
 		end
 
 		def line_count( file )
@@ -31,7 +36,7 @@ module Hangman
 			solution = ""
 			while !solution.length.between?(5, 12)
 				line_number = random_line(line_count("5desk.txt"))
-				solution = get_phrase_from_file( "5desk.txt", line_number).strip
+				solution = get_phrase_from_file( "5desk.txt", line_number).strip.downcase
 			end
 			solution
 		end
@@ -71,24 +76,46 @@ module Hangman
 		def parse_entry ( guess_letter )
 			if guess_letter == "save"
 				return :save
+			elsif guess_letter == "load"
+				return :load
 			elsif incorrect_guesses.include?(guess_letter) || status.include?(guess_letter)
 				puts "Already guessed that letter, dingus!"
 				return :invalid
-			elsif guess_letter.length > 1
-				puts "Only one letter, dingus!"
+			elsif guess_letter.length != 1
+				puts "One letter, dingus!"
 				return :invalid
 			end
 			return 
 		end
 
 		def save_game
+			file_name = File.open("save_data.yaml", "w")
+			YAML.dump(self, file_name)
+			file_name.close
+		end
 
+		def load_game
+			file_name = File.open("save_data.yaml", "r")
+			puts "Choose game to load: "
+			loaded_game = YAML.load(file_name)
+			loaded_game_hash = {
+				:word_to_guess => loaded_game.word_to_guess,
+				:status => loaded_game.status,	
+				:remaining_turns => loaded_game.remaining_turns,
+				:incorrect_guesses => loaded_game.incorrect_guesses
+			}
+			initialize(loaded_game_hash)
 		end
 
 		def begin_game
 			loop do
+				if out_of_turns?
+					puts word_to_guess
+					puts "You Lose."
+					break
+				end
 				self.print_status
-				print "Enter 'save' to save your game. \nGuess a letter (incorrect guesses: #{incorrect_guesses.join(",")}): "
+				print "Try 'save' or 'load'. \nGuess a letter (incorrect guesses: #{incorrect_guesses.join(",")}): "
 				entry = gets.chomp.downcase
 
 				case parse_entry(entry)
@@ -96,6 +123,9 @@ module Hangman
 					next
 				when :save
 					self.save_game
+					break
+				when :load
+					self.load_game
 					break
 				end
 
@@ -111,13 +141,6 @@ module Hangman
 					puts "You Win!"
 					break
 				end
-
-				if out_of_turns?
-					puts word_to_guess
-					puts "You Lose."
-					break
-				end
-
 			end
 		end
 
